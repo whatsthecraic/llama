3aae1be354a65494ac3d54c1231de844587a3102
Dean De Leo <deandeleo@hotmail.co.uk>
2020-05-23 00:16:59 +0200

    Disable LLAMA_DEBUG

--- llama/include/llama/ll_common.h
+++ llama/include/llama/ll_common.h
@@ -459,9 +459,7 @@ typedef unsigned length_t;
 
 #endif
 
-#include <iostream>
-#include <mutex>
-extern std::mutex _log_mutex [[maybe_unused]];
-#define LLAMA_DEBUG(msg) { std::scoped_lock<std::mutex> lock{::_log_mutex}; std::cout << "[LLAMA (internal), " << __FILE__ << "::" << __LINE__ << ", " << __FUNCTION__ << "] " << msg << std::endl; }
-
-
+//#include <iostream>
+//#include <mutex>
+//extern std::mutex _log_mutex [[maybe_unused]];
+//#define LLAMA_DEBUG(msg) { std::scoped_lock<std::mutex> lock{::_log_mutex}; std::cout << "[LLAMA (internal), " << __FILE__ << "::" << __LINE__ << ", " << __FUNCTION__ << "] " << msg << std::endl; }
